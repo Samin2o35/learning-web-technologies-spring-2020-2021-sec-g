26700fa0f07e58663323e139c93c25c035a84090
Samin2o35 <60335026+Samin2o35@users.noreply.github.com>
2021-02-16 11:01:21 +0600

    Task 3_B added

---
+++ MID_LAB_TASK_PHP_05/TASK_3/Task_B/myDOB.php
@@ -0,0 +1,29 @@
+<?php
+
+	if(isset($_POST['submit'])){
+
+		$DOB		=  $_POST['myDOB'];
+	
+		if($DOB == ""){
+			echo "Null submission...";
+		}else{
+			echo $DOB;
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DOB Input field</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset>
+			<legend><b>DATE OF BIRTH</b></legend>
+			Date of Birth : 		<input type="DOB" name="myDOB" value=""> <br>
+						<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
