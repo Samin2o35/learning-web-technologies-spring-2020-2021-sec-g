2fafbd6b02786fc8f6b5ce5f3f2789074e6c323d
Samin2o35 <60335026+Samin2o35@users.noreply.github.com>
2021-02-16 08:01:49 +0600

    Task 1 added

---
+++ MID_LAB_TASK_PHP_04/Task 1.php
@@ -0,0 +1,8 @@
+<?php  
+    //Find area and perimeter of rectangle
+    $length = 10;  
+    $width = 15;
+
+    echo "Area of rectangle: " . ($length * $width) . "\n";
+    echo "Perimeter of rectangle: " . 2 * ($length + $width);   
+  ?>  
