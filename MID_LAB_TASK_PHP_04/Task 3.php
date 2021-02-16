225ece29f803a9ef8bc77447fe5e8e2890b2e0c2
Samin2o35 <60335026+Samin2o35@users.noreply.github.com>
2021-02-16 08:21:49 +0600

    Task 3 added

---
+++ MID_LAB_TASK_PHP_04/Task 3.php
@@ -0,0 +1,14 @@
+<?php  
+    //Find if number even or odd
+    $number = 25;  
+
+    if( $number%2 == 0){
+
+      echo "The number is even";
+    }
+    
+    else {
+
+      echo "The number is odd";
+    }
+  ?>  
