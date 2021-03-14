84ef8bfe595c396221d98f08929d5812b8b13c29
Samin2o35 <60335026+Samin2o35@users.noreply.github.com>
2021-03-14 11:18:44 +0600

    3rd Commit

--- MID_LAB_EXAM/controller/regCheck.php
+++ MID_LAB_EXAM/controller/regCheck.php
@@ -1,3 +1,19 @@
 <?php
+	session_start();
+
+	if(isset($_POST['signup'])){
+
+		$name = $_POST['name'];
+		$password = $_POST['password'];
+		$repass = $_POST['repass'];
+		$id = $_POST['id'];
+
+		if($name == "" || $password == "" || $id == ""){
+			echo "null value found...";
+		}else{
+			
+		}
+	}
+
 
 ?>
